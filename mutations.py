ecb3facb2d51c8be30f31380f55cf691ad39844c
Alex Kiura <kiuraalex@gmail.com>
2021-01-12 12:31:52 +0300

    Bind to db before creation

--- mutations.py
+++ mutations.py
@@ -3,7 +3,7 @@ import os
 import aioredis
 from ariadne import ObjectType, convert_kwargs_to_snake_case
 
-from models import Message, User
+from models import Message, User, db, database_url
 
 mutation = ObjectType("Mutation")
 
@@ -15,15 +15,16 @@ async def resolve_create_message(obj, info, content, sender_id, recipient_id):
         redis_url = os.getenv("REDIS_URL")
         channel_name = "MESSAGES"
         publisher = await aioredis.create_redis(redis_url)
-        message = await Message.create(content=content, sender_id=sender_id,
-                                       recipient_id=recipient_id)
-        serialized_message = message.to_dict()
-        await publisher.publish_json(channel_name, serialized_message)
-        publisher.close()
-        return {
-            "success": True,
-            "message": serialized_message
-        }
+        async with db.with_bind(database_url) as engine:
+            message = await Message.create(content=content, sender_id=sender_id,
+                                           recipient_id=recipient_id)
+            serialized_message = message.to_dict()
+            await publisher.publish_json(channel_name, serialized_message)
+            publisher.close()
+            return {
+                "success": True,
+                "message": serialized_message
+            }
     except Exception as error:
         return {
             "success": False,
@@ -35,11 +36,12 @@ async def resolve_create_message(obj, info, content, sender_id, recipient_id):
 @convert_kwargs_to_snake_case
 async def resolve_create_user(obj, info, username):
     try:
-        user = await User.create(username=username)
-        return {
-            "success": True,
-            "user": user.to_dict()
-        }
+        async with db.with_bind(database_url) as engine:
+            user = await User.create(username=username)
+            return {
+                "success": True,
+                "user": user.to_dict()
+            }
     except Exception as error:
         return {
             "success": False,
