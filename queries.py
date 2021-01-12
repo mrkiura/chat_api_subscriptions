bdf25fe0ed21d2362c53843b13bdcb453e749382
Alex Kiura <kiuraalex@gmail.com>
2021-01-12 12:32:14 +0300

    Bind to db before retrieving messages

--- queries.py
+++ queries.py
@@ -1,6 +1,6 @@
 from ariadne import ObjectType, convert_kwargs_to_snake_case
 
-from models import Message
+from models import Message, db, database_url
 
 query = ObjectType("Query")
 
@@ -8,9 +8,11 @@ query = ObjectType("Query")
 @query.field("messages")
 @convert_kwargs_to_snake_case
 async def resolve_messages(obj, info, user_id):
-    messages = await Message.query.where((Message.sender_id == user_id) | (
-            Message.recipient_id == user_id)).gino.all()
-    return {
-        "success": True,
-        "messages": messages
-    }
+    async with db.with_bind(database_url) as engine:
+        messages = await Message.query.where((Message.sender_id == user_id) | (
+                Message.recipient_id == user_id)).gino.all()
+        return {
+            "success": True,
+            "messages": messages
+        }
+
