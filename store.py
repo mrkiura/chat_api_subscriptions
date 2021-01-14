0454cbacc8c832808db0bdb0f8098121eb2e6ab9
Alex Kiura <kiuraalex@gmail.com>
2021-01-14 09:52:23 +0300

    Add helper to create users

--- store.py
+++ store.py
@@ -4,3 +4,16 @@ messages = []
 users = {}
 
 queue = asyncio.Queue()
+
+
+def create_user(username):
+    if not users.get(username):
+        user = {
+            "user_id": len(users) + 1,
+            "username": username
+        }
+        users[username] = user
+        return {
+            "success": True,
+            "user": user
+        }
